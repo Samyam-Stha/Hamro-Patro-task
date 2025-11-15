b4c4ba24981c6bbf58ae1efb7df65226253b4335
Samyam Shrestha <samyamsth2@gmail.com>
2025-11-15 17:47:12 +0545

    fetched ingredients and measurements and displayed

--- src/Pages/DetailsPage/Details.jsx
+++ src/Pages/DetailsPage/Details.jsx
@@ -18,7 +18,43 @@ export default function Details() {
   }, [id]);
   if (!meal) return <h2>Loading</h2>;
 
+  const steps = meal.strInstructions
+    .split("\n")
+    .filter((step) => step.trim() !== "");
 
+  function getIngredients(meal) {
+    const ingArr = [];
+
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = meal[`strIngredient${i}`];
+
+      if (ingredient) {
+        ingArr.push(ingredient);
+      } else {
+        break;
+      }
+    }
+
+    return ingArr;
+  }
+  function getMeasurements(meal) {
+    const mesArr = [];
+
+    for (let i = 1; i <= 20; i++) {
+      const measurement = meal[`strMeasure${i}`];
+
+      if (measurement) {
+        mesArr.push(measurement);
+      } else {
+        break;
+      }
+    }
+
+    return mesArr;
+  }
+
+  const items = getIngredients(meal);
+  const measures = getMeasurements(meal);
 
   return (
     <section className={styles.detailbox}>
@@ -36,7 +72,30 @@ export default function Details() {
             <p>Area: {meal.strArea}</p>
           </div>
         </div>
-       
+        <hr></hr>
+        <br></br>
+        <h3>Ingredients</h3>
+        {items.map((item, index) => (
+          <ul>
+            <li key={index}>
+              {item}: {measures[index]}
+            </li>
+          </ul>
+        ))}
+        <br></br>
+        <hr></hr>
+        <br></br>
+        <h3>Instructions:</h3>
+        <span>
+          <a href={meal.strYoutube} target="_blank">
+            Tutorial Video
+          </a>
+        </span>
+        <ol>
+          {steps.map((step, index) => (
+            <li key={index}>{step}</li>
+          ))}
+        </ol>
       </div>
     </section>
   );
